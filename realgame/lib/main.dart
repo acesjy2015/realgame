f550b0390919bf7912abc55243f35ab6649451cc
acesjy2015 <35905696+acesjy2015@users.noreply.github.com>
2023-07-13 10:58:33 +0900

    3차

--- realgame/lib/main.dart
+++ realgame/lib/main.dart
@@ -76,9 +76,11 @@ class App extends StatelessWidget {
 */
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'card.dart';
+import 'widgets/card.dart';
 
-void main() => runApp(const App());
+void main() => runApp(const MaterialApp(
+      home: App(),
+    ));
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -89,26 +91,68 @@ class App extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(0.0),
+          preferredSize: const Size.fromHeight(40),
           child: AppBar(
             centerTitle: true,
-            title: const Text(""),
-            backgroundColor: Colors.transparent,
-            elevation: 0.0,
+            title: const Text("Game"),
+            backgroundColor: Colors.black54,
+            elevation: 0,
+            actions: [
+              IconButton(
+                onPressed: () {
+                  showDialog(
+                    barrierDismissible: true,
+                    context: context,
+                    builder: ((context) {
+                      return AlertDialog(
+                        title: const Text("설정"),
+                        content: const Text("테스트"),
+                        actions: [
+                          Container(
+                            child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text("창 닫기"),
+                            ),
+                          )
+                        ],
+                      );
+                    }),
+                  );
+                },
+                icon: const Icon(Icons.settings),
+              )
+            ],
           ),
         ),
 
-        //-------------------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------------- 여기부터 본체
+
         body: Column(
           children: [
             Flexible(
               flex: 1,
-              child: Container(
-                  decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/images/testimage.jpg'),
-                ),
-              )),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(
+                    child: Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            fit: BoxFit.fill,
+                            image: AssetImage('assets/images/testimage2.png'),
+                          ),
+                        )),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                ],
+              ),
             ),
             Flexible(
               flex: 1,
@@ -116,9 +160,10 @@ class App extends StatelessWidget {
                 children: [
                   Expanded(
                     child: Container(
-                      decoration: const BoxDecoration(color: Colors.blue),
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      decoration: const BoxDecoration(color: Color(0xffeeeeee)),
                       child: const Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.end,
                         mainAxisAlignment: MainAxisAlignment.start,
                         children: [
                           Expanded(
@@ -136,7 +181,7 @@ class App extends StatelessWidget {
             Flexible(
               flex: 2,
               child: Container(
-                decoration: const BoxDecoration(color: Colors.green),
+                decoration: const BoxDecoration(color: Color(0xffeeeeee)),
                 child: const Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
@@ -144,18 +189,9 @@ class App extends StatelessWidget {
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          SizedBox(
-                            height: 3,
-                          ),
-                          SelectCard(line: "약을 받는다."),
-                          SizedBox(
-                            height: 3,
-                          ),
-                          SelectCard(line: "음식 캔을 받는다."),
-                          SizedBox(
-                            height: 3,
-                          ),
-                          SelectCard(line: "물을 받는다."),
+                          BotborderSelectCard(line: "약을 받는다."),
+                          BotborderSelectCard(line: "통조림을 받는다."),
+                          NoneborderSelectCard(line: "물을 받는다."),
                           SizedBox(
                             height: 3,
                           ),
